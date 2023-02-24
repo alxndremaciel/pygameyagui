0dfb5b0dce09c40874340b448f0eacb058a524c8
Alexandre Maciel <103848895+alxndremaciel@users.noreply.github.com>
2023-02-24 13:50:15 -0300

    Testing autodoc_mock_imports

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,13 +2,13 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-import os
-import sys
+#import os
+#import sys
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath("."))
-sys.path.insert(0, os.path.abspath("../"))
+#sys.path.insert(0, os.path.abspath("."))
+#sys.path.insert(0, os.path.abspath("../"))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -27,6 +27,7 @@ extensions = [
     'sphinx.ext.autodoc',
 ]
 
+autodoc_mock_imports = ['pygameyagui']
 templates_path = ['_templates']
 exclude_patterns = []
 
